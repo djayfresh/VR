3ebce54b458bccfa5110bbce9747764adb6dbff9
djayfresh <djfresh1@comcast.net>
2016-04-15 07:45:15 -0500

    Working with new Hands

---
+++ Leap/LeapMotion/Assets/Scripts/Leap/HandManager.cs
@@ -0,0 +1,33 @@
+﻿using Game.Manager;
+using Leap.Unity;
+
+namespace Game.Manager
+{
+    using UnityEngine;
+
+    public class HandManager : MonoBehaviour
+    {
+
+    }
+
+    public enum HandState
+    {
+        NotTracking,
+        Down,
+        Up,
+        Left,
+        Right,
+        Forward,
+        Back
+    }
+
+    public enum FingersState
+    {
+        NotTracked,
+        Open,
+        Closed,
+        Pinch,
+        Pointing,
+        Grasp
+    }
+}
---
+++ Leap/LeapMotion/Assets/Scripts/Leap/HandModel.cs
@@ -0,0 +1,37 @@
+﻿using Game.Manager;
+using Leap.Unity;
+using UnityEngine;
+
+namespace Game.Leap
+{
+    public class HandModel : RigidHand
+    {
+        public HandState HandState = HandState.NotTracking;
+        public FingersState FingersState = FingersState.NotTracked;
+        public Vector3 UP = Vector3.up;
+        
+        public override void InitHand()
+        {
+            base.InitHand();
+        }
+
+        public override void UpdateHand()
+        {
+            base.UpdateHand();
+
+            Vector3 handNormal = GetPalmNormal();
+            float dotNormal = Vector3.Dot(handNormal, UP);
+
+            if(dotNormal > 0.5)
+            {
+                HandState = HandState.Up;
+            }
+            if(dotNormal < -0.5)
+            {
+                HandState = HandState.Down;
+            }
+        }
+    }
+
+
+}
